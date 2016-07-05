22bd416a80d8d4739be935dc5cc4fef2198247d2
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-07-05 10:57:35 -0700

    Update steady.sh

--- steady.sh
+++ steady.sh
@@ -32,7 +32,7 @@ cat << EOF
  
 EOF
 echo -e "               \e[100m                Steady script            \e[00;37;40m"
-echo -e "               \e[01;34m            by shield team              \e[00;37;40m"
+echo -e "               \e[01;34m            by Turbo team              \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
@@ -59,9 +59,9 @@ sleep 0.5
 
 echo ""
 echo -e "\033[38;5;208m                                              \033[0;00m"
-echo -e "\033[38;5;208m             shield team                      \033[0;00m"
-echo -e "\033[38;5;208m             @shieldTM                        \033[0;00m"
-echo -e "\033[38;5;208m          developer : @hafez1116hafez         \033[0;00m"
+echo -e "\033[38;5;208m             Turbo team                      \033[0;00m"
+echo -e "\033[38;5;208m             TurboTM                        \033[0;00m"
+echo -e "\033[38;5;208m          developer : @armanturbo         \033[0;00m"
 echo -e "\033[38;5;208m                                              \033[0;00m"
 
 sleep 1.5
@@ -190,7 +190,7 @@ cat << EOF
  
 EOF
 echo -e "                \e[100m                Steady script           \e[00;37;40m"
-echo -e "               \e[01;34m                    by shield team     \e[00;37;40m"
+echo -e "               \e[01;34m                    by Turbo team     \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
@@ -217,9 +217,9 @@ sleep 0.5
 
 echo ""
 echo -e "\033[38;5;208m                                              \033[0;00m"
-echo -e "\033[38;5;208m        shield team                           \033[0;00m"
-echo -e "\033[38;5;208m          @shieldTM                           \033[0;00m"
-echo -e "\033[38;5;208m          developer : @hafez1116hafez         \033[0;00m"
+echo -e "\033[38;5;208m        Turbo team                           \033[0;00m"
+echo -e "\033[38;5;208m          TurboTM                           \033[0;00m"
+echo -e "\033[38;5;208m          developer : @armanturbo         \033[0;00m"
 echo -e "\033[38;5;208m                                              \033[0;00m"
 
 # Starting preliminar setup
@@ -503,9 +503,9 @@ while getopts ":tsTSih" opt; do
 	echo ""
 	echo -e "\e[0m"
 echo -e "\033[38;5;208m                                            \033[0;00m"
-echo -e "\033[38;5;208m        shield team                         \033[0;00m"
-echo -e "\033[38;5;208m         @shieldTM                          \033[0;00m"
-echo -e "\033[38;5;208m        developer : @hafez1116hafez\033[0;00m"
+echo -e "\033[38;5;208m        Turbo team                         \033[0;00m"
+echo -e "\033[38;5;208m         TurboTM                          \033[0;00m"
+echo -e "\033[38;5;208m        developer : @armanturbo\033[0;00m"
 echo -e "\033[38;5;208m                                              \033[0;00m"	
 echo ""
 	exit 1
