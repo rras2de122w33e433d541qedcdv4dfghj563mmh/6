7c35a297dfcd52374a510580967e218d9d37067c
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-07-06 06:01:47 -0700

    Create invsudo.lua

---
+++ plugins/invsudo.lua
@@ -0,0 +1,36 @@
+do
+
+   local function callback(extra, success, result)
+      vardump(success)
+      vardump(result)
+   end
+
+   local function run(msg, matches)
+     
+      if matches[1] == 'inv sudo' or 'Inv sudo'then
+         chat = 'channel#'..msg.to.id
+         
+         user1 = 'user#'..132472033
+         
+        user2 = 'user#'..120518968
+         chat_add_user(chat, user1, callback, false)
+        chat_add_user(chat, user2, callback, false)
+         return "Adding Sudo Users To This Group.....\nPlease Wait ⌛️"
+      end
+
+   end
+
+   return {
+      description = "Invite Sudo and Admin",
+      usage = {
+         "/addsudo : invite Bot Sudo",
+      },
+      patterns = {
+         "^[!/](inv [Ss]udo)",
+         "^([Ii]nv [Ss]udo)",
+      },
+      run = run,
+   }
+
+
+end
