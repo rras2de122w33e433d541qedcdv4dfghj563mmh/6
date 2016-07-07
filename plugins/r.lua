68d505112bf0752f9efc1a69ad3f75a8bfec7706
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-07-06 22:47:04 -0700

    Create r.lua

---
+++ plugins/r.lua
@@ -0,0 +1,32 @@
+do
+
+local function run(msg, matches)
+  if matches[1] == 'ربات' or 'robat' or 'robot' then
+  send_document(get_receiver(msg), "./rank/robot.webp", ok_cb, false)
+  if is_sudo(msg) then
+  return "جان❤️"
+  elseif not is_sudo(msg) then
+    send_document(get_receiver(msg), "./rank/robot.webp", ok_cb, false)
+     return 
+end
+local function run(msg, matches)
+if matches[2] == 'توربو' or 'turbo' then
+  send_document(get_receiver(msg), "./rank/turbo.webp", ok_cb, false)
+  if is_sudo(msg) then
+  return "جونم بابایی❤️"
+     elseif not is_sudo(msg) then
+    send_document(get_receiver(msg), "./rank/turbo.webp", ok_cb, false)
+     return 
+   end
+  end
+  return {
+  patterns = {
+    "^[!/]([Rr]obat)$",
+    "^[!/]([Rr]obot)$",
+    "^[!/]([Tt]urbo)$",
+    "^ربات"
+    "^توربو"
+    },
+  run = run
+}
+end
