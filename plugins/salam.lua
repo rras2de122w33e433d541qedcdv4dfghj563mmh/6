a02372af94af832ad3d404ff12eb07de5350252d
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-07-08 05:45:11 -0700

    Update salam.lua

--- plugins/salam.lua
+++ plugins/salam.lua
@@ -6,11 +6,11 @@ local mina = 132472033
      if not value then
         if msg.from.id == tonumber(mina) then 
            text = text..'❤️سلام بهترین بابای دنیا💖\n\nTurbo ™'
-         elseif is_admin(msg.from.id) then
+         elseif is_admin2(msg.from.id) then
            text = text..'💜سلام   نخودی💚 \n\n'
-         elseif is_owner(msg.from.id, msg.to.id) then
+         elseif is_owner2(msg.from.id, msg.to.id) then
            text = text..'❣سلام مدیر جون❣ \n\nTurbo ™'
-         elseif is_momod(msg.from.id, msg.to.id) then
+         elseif is_momod2(msg.from.id, msg.to.id) then
            text = text..'💘سلام معاون💞 \n\nTurbo ™'
      else
            text = text..'سلام\n\nTurbo ™'
